0daaa8e7c6cdeb1832bad87e6aa4667e40a027c8
Wojciech <wojciech.kur96@gmail.com>
2018-03-16 02:49:45 +0100

    FileReader works together with Point class
    Point class stores each element of maze as coordinates of point and which wall exists

--- Sources/FileReader.py
+++ Sources/FileReader.py
@@ -1,7 +1,29 @@
-
+from Sources.Point import *
 
 class FileReader:
     def __init__(self, filename):
         self.__filename = open(filename)
+        self.__maze_map = []
 
+    def read(self):
+        file = self.__filename.read().split('\n')
+        for var in file:
+            point = Point()
+            var = var.split()
+            point.x = var[0]
+            point.y = var[1]
 
+            if var.__len__() == 3:
+                if 'P' in var[2]:
+                    point.right_wall = True
+                if 'D' in var[2]:
+                    point.buttom_wall = True
+
+            self.__maze_map.append(point)
+
+    def get_map(self):
+        return self.__maze_map
+
+    def print_map(self):
+        for point in self.__maze_map:
+            print("(" + point.x + "," + point.y + ") - P: " + str(point.right_wall) + " D: " + str(point.buttom_wall) + "\n")
---
+++ Sources/Point.py
@@ -0,0 +1,9 @@
+
+
+class Point:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.right_wall = False
+        self.buttom_wall = False
+
